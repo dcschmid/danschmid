253f3de198e97e13b29961668aa4f16f94c31011
Daniel Schmid <daniel.schmid@libertymail.net>
2020-11-28 18:08:19 +0100

    fix: header and navigation styles

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -11,7 +11,12 @@
     ?>
     <link rel="me" href="https://twitter.com/dcschmid1">
     <link rel="me" href="https://github.com/dcschmid">
+    <link rel="alternate" type="application/rss+xml" title="Latest articles" href="<?= site()->url() ?>/feed"/>
   </head>
   <body>
     <?php snippet('svgsprite') ?>
-    <?php snippet('mainnav') ?>
+    <header class="nav-wrapper">
+      <?php snippet('mainnav') ?>
+    </header>
+    <main class="main-body">
+    <?php commentions('feedback') ?>
