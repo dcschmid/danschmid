c219ad060276dfe64d60c2867491de171d5c6e68
Daniel Schmid <daniel.schmid@libertymail.net>
2021-01-05 14:40:47 +0100

    feat: activate navigation js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
-//import navigation from "./modules/navigation";
+import navigation from "./modules/navigation";
 import lozad from "lozad";
+
+navigation();
 
 const el = document.querySelectorAll("img[data-lazyload]");
 const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
