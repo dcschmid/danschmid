eee00dbb7e7930085096bc52073174c56ff6ce39
Daniel Schmid <daniel.schmid@libertymail.net>
2021-01-05 08:21:51 +0100

    feat: add syntax highlight

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
-// import a plugin
-import Prism from "prismjs";
 //import navigation from "./modules/navigation";
 import lozad from "lozad";
-
-//navigation();
-Prism.highlightAll();
 
 const el = document.querySelectorAll("img[data-lazyload]");
 const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
