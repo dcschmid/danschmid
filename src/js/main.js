024c488f3762a76650109c676e32a81fac4455da
Daniel Schmid <daniel.schmid@libertymail.net>
2021-01-05 17:06:36 +0100

    fix: navigation.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import navigation from "./modules/navigation";
 import lozad from "lozad";
 
-navigation();
-
 const el = document.querySelectorAll("img[data-lazyload]");
 const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
 observer.observe();
+
+navigation();
