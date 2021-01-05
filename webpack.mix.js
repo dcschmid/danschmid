a8d0f53b9fc40330dd98f3f1fa6ac55857562b48
Daniel Schmid <daniel.schmid@libertymail.net>
2021-01-05 15:36:18 +0100

    fix: improve webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,19 +2,14 @@ const mix = require("laravel-mix");
 mix.setPublicPath("assets");
 mix.browserSync("kirby.local");
 
-if (mix.inProduction()) {
-  mix.version();
-}
-
 mix
-  .sourceMaps()
   .js("src/js/main.js", "assets/js")
   .sass("src/scss/main.scss", "assets/css")
   .options({
     autoprefixer: {
       options: {
-        browsers: ["last 2 versions"],
-      },
-    },
+        browsers: ["last 2 versions"]
+      }
+    }
   })
-  .extract();
+  .version();
