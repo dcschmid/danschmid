adb04548c3ebf117857f02d2876d678cd86fb05e
Daniel Schmid <daniel.schmid@libertymail.net>
2021-01-26 18:20:58 +0100

    feat: add favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,6 +14,16 @@
     <link rel="webmention" href="https://webmention.io/danschmid.de/webmention" />
     <link rel="pingback" href="https://webmention.io/danschmid.de/xmlrpc" />
     <link rel="alternate" type="application/rss+xml" title="Latest articles" href="<?= site()->url() ?>/feed"/>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+    <meta name="apple-mobile-web-app-title" content="Danschmid">
+    <meta name="application-name" content="Danschmid">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#000000">
   </head>
   <body>
     <?php snippet('svgsprite') ?>
