9590a000d50f9de07d4d8bc8c6f763a7fe7dfd5e
Daniel Schmid <daniel.schmid@libertymail.net>
2021-01-30 19:11:17 +0100

    fix: add related articles to default template

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,6 +4,15 @@
   <h1 class="article__headline"><?= $page->title()->html() ?></h1>
 
   <?= $page->text()->kt() ?>
+
+
+  <?php
+    $related = $page->related()->toPages();
+    if ($related->count() > 0):
+  ?>
+    <?php snippet('related', ['related' => $related]) ?>
+  <?php endif ?>
+
 </article>
 
 
